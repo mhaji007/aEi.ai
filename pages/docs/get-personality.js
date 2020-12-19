587ad8ac19498a923ec75ca9f7e6a5824d824a1b
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-12-19 11:51:27 -0500

    Set up get personality

--- pages/docs/get-personality.js
+++ pages/docs/get-personality.js
@@ -4,34 +4,34 @@ import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
 import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
 import JSONPretty from "react-json-prettify";
 
-function GetMood() {
+function GetPersonality() {
   return (
     <>
-      {/* <!--Get User's Mood Model--> */}
-      <div id="api_rest_get_mood" className="card">
+      {/* <!--Get User's Personality Model--> */}
+      <div id="api_rest_get_personality" className="card">
         <div
           className="card-header card-header-secondary"
           style={{ background: "linear-gradient(60deg, #391141, #8e24aa)" }}
         >
-          <h3 id="h3_get_mood_1" className="my-0 text-white">
-            Get User's Mood Model
+          <h3 id="h3_get_pers_1" className="my-0 text-white">
+            Get User's Personality Model
           </h3>
         </div>
-
         <div className="card-body">
-          <p id="p_get_mood_1" className="mt-2 ml-4 mr-4 text-justify">
-            At any point during or after an interaction, users' mood model can
-            be queried from the aEi.ai service. If you need incremental updates
-            of a user's mood model during an interaction, it should be queried
-            after every new input to the interaction.
+          <p id="p_get_pers_1" className="mt-2 ml-4 mr-4 text-justify">
+            At any point during or after an interaction, users' personality
+            model can be queried from the aEi.ai service. If you need
+            incremental updates of a user's personality model during an
+            interaction, it should be queried after every new input to the
+            interaction.
           </p>
-          <p id="p_get_mood_2" className="mt-2 ml-4 mr-4 text-justify">
-            You can query a user's mood model by making a{" "}
+          <p id="p_get_pers_2" className="mt-2 ml-4 mr-4 text-justify">
+            Query a user's personality model by making a{" "}
             <strong>
               <u>GET</u>
             </strong>{" "}
-            request to the <strong>get_user_mood</strong> API with your target
-            user ID as path variable.
+            request to the <strong>get_user_personality</strong> API with your
+            target user ID as path variable.
           </p>
           <div className="card-body">
             <div className="d-flex justify-content-center">
@@ -58,11 +58,12 @@ function GetMood() {
                 </Thead>
                 <Tbody>
                   <Tr>
-                    <Td id="td_get_mood_1">PAID</Td>
-                    <Td id="td_get_mood_2">GET</Td>
-                    <Td id="td_get_mood_3" colspan="2">
+                    <Td id="td_get_pers_1">PAID</Td>
+                    <Td id="td_get_pers_2">GET</Td>
+                    <Td id="td_get_pers_3" colspan="2">
                       <strong>
-                        https://aei.ai/api/v1/users/{"{"}user_id{"}"}/mood
+                        https://aei.ai/api/v1/users/{"{"}user_id{"}"}
+                        /personality
                       </strong>
                     </Td>
                   </Tr>
@@ -70,7 +71,6 @@ function GetMood() {
               </Table>
             </div>
           </div>
-
           <div className="card-body">
             <div className="d-flex justify-content-center">
               <Table className="table table-responsive table-striped w-auto rounded-top rounded-bottom border">
@@ -99,21 +99,21 @@ function GetMood() {
                 </Thead>
                 <Tbody>
                   <Tr>
-                    <Td id="td_get_mood_4">Authorization</Td>
-                    <Td id="td_get_mood_5">Header</Td>
-                    <Td id="td_get_mood_6">Yes</Td>
-                    <Td id="td_get_mood_7">
+                    <Td id="td_get_pers_4">Authorization</Td>
+                    <Td id="td_get_pers_5">Header</Td>
+                    <Td id="td_get_pers_6">Yes</Td>
+                    <Td id="td_get_pers_7">
                       Value should be '
                       <strong>Bearer &lt;your access_token&gt;</strong>'
                     </Td>
                   </Tr>
                   <Tr>
-                    <Td id="td_get_mood_8">user_id</Td>
-                    <Td id="td_get_mood_9" className="text-nowrap">
+                    <Td id="td_get_pers_8">user_id</Td>
+                    <Td id="td_get_pers_9" className="text-nowrap">
                       Path Variable
                     </Td>
-                    <Td id="td_get_mood_10">Yes</Td>
-                    <Td id="td_get_mood_11">
+                    <Td id="td_get_pers_10">Yes</Td>
+                    <Td id="td_get_pers_11">
                       Unique user ID of the target user
                     </Td>
                   </Tr>
@@ -146,47 +146,51 @@ function GetMood() {
                 </Thead>
                 <Tbody>
                   <Tr>
-                    <Td id="td_get_mood_12">status</Td>
-                    <Td id="td_get_mood_13">
+                    <Td id="td_get_pers_12">status</Td>
+                    <Td id="td_get_pers_13">
                       <a href="#error_handling">Status</a>
                     </Td>
-                    <Td id="td_get_mood_14" colspan="2">
+                    <Td id="td_get_pers_14" colspan="2">
                       Response <a href="#error_handling">status</a> object
                     </Td>
                   </Tr>
                   <Tr>
-                    <Td id="td_get_mood_15">userId</Td>
-                    <Td id="td_get_mood_16">String</Td>
-                    <Td id="td_get_mood_17" colspan="2">
+                    <Td id="td_get_pers_15">userId</Td>
+                    <Td id="td_get_pers_16">String</Td>
+                    <Td id="td_get_pers_17" colspan="2">
                       Unique user ID of the target user
                     </Td>
                   </Tr>
                   <Tr>
-                    <Td id="td_get_mood_18">mood</Td>
-                    <Td id="td_get_mood_19">
-                      <a href="#data_structures_mood">Mood</a>
+                    <Td id="td_get_pers_18">personality</Td>
+                    <Td id="td_get_pers_19">
+                      <a href="#data_structures_personality">Personality</a>
                     </Td>
-                    <Td id="td_get_mood_20" colspan="2">
-                      <a href="#data_structures_mood">Mood model</a> of the
-                      target user
+                    <Td id="td_get_pers_20" colspan="2">
+                      <a href="#data_structures_personality">
+                        Personality model
+                      </a>{" "}
+                      of the target user
                     </Td>
                   </Tr>
                 </Tbody>
               </Table>
             </div>
           </div>
-          {/* <!--Sample code to query a user's mood model--> */}
-          <p id="p_get_mood_3" className="mt-2 ml-4 mr-4 text-justify">
-            An example of mood model query looks like the following:
+
+          {/* <!--Sample code to query a user's personality model--> */}
+          <p id="p_get_pers_3" className="mt-2 ml-4 mr-4 text-justify">
+            Knowing the access token, you can query user's personality model as
+            follows:
           </p>
-          <p id="p_get_mood_4">
+          <p id="p_get_pers_4" className="mt-2 ml-4 mr-4 text-justify">
             <code className="prettyprint">
               curl -X GET 'https://aei.ai/api/v1/users/&lt;target
-              user_id&gt;/mood' -H 'Authorization: Bearer &lt;your
+              user_id&gt;/personality' -H 'Authorization: Bearer &lt;your
               access_token&gt;'
             </code>
           </p>
-          <p id="p_get_mood_5" className="mt-2 ml-4 mr-4 text-justify">
+          <p id="p_get_pers_5" className="mt-2 ml-4 mr-4 text-justify">
             Expected response from aEi.ai would look like this:
             <JSONPretty
               json={{
@@ -196,13 +200,19 @@ function GetMood() {
                   help: null,
                 },
                 userId: "fcc60b6e-264d-4df9-83d2-346a3799c688",
-                mood: {
+                personality: {
                   pad: {
                     pleasure: 0.1,
                     arousal: 0.1,
                     dominance: 0.1,
                   },
-                  moodName: "EXUBERANT",
+                  bigFive: {
+                    openness: 0.01,
+                    conscientiousness: 0.01,
+                    extroversion: 0.01,
+                    agreeableness: 0.01,
+                    neuroticism: 0.01,
+                  },
                 },
               }}
             />
@@ -212,5 +222,6 @@ function GetMood() {
     </>
   );
 }
-GetMood.Layout = DocsLayout;
-export default GetMood;
+
+GetPersonality.Layout = DocsLayout;
+export default GetPersonality;
