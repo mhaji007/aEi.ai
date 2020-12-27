eb14fce242cfaa9f6c42358d6caf5d3c65777b02
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-12-27 14:42:07 -0500

    Set up free queries

---
+++ pages/docs/free-queries.js
@@ -0,0 +1,172 @@
+import React from "react";
+import DocsLayout from "../../components/layouts/docs";
+import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
+import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
+import JSONPretty from "react-json-prettify";
+
+function FreeQueries() {
+  return (
+    <>
+      {/* <!--Get Number of Used Free Queries--> */}
+      <div id="api_rest_get_used_free_queries" className="card">
+        <div
+          className="card-header card-header-secondary"
+          style={{ background: "linear-gradient(60deg, #391141, #8e24aa)" }}
+        >
+          <h3 id="h3_free_q_1" className="my-0 text-white">
+            Get Number of Used Free Queries
+          </h3>
+        </div>
+        <div className="card-body">
+          <p id="p_free_q_1" className="mt-2 ml-4 mr-4 text-justify">
+            You can get number of used queries out of 10,000 free queries in
+            current month by making a{" "}
+            <strong>
+              <u>GET</u>
+            </strong>{" "}
+            request to the metrics/queries/used API.
+          </p>
+          <div className="card-body">
+            <div className="d-flex justify-content-center">
+              <Table className="table table-responsive table-striped w-auto rounded-top rounded-bottom border">
+                <Thead className="thead-dark">
+                  <Tr>
+                    <Th colspan="4" className="text-center">
+                      <strong>Request</strong>
+                    </Th>
+                  </Tr>
+                </Thead>
+                <Thead className="thead-light">
+                  <Tr>
+                    <Th className="text-nowrap">
+                      <strong>API Type</strong>
+                    </Th>
+                    <Th className="text-nowrap">
+                      <strong>HTTP Method</strong>
+                    </Th>
+                    <Th colspan="2">
+                      <strong>URL</strong>
+                    </Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td id="td_free_q_1">FREE</Td>
+                    <Td id="td_free_q_2">GET</Td>
+                    <Td id="td_free_q_3" colspan="2">
+                      <strong>
+                        https://aei.ai/api/v1/metrics/queries/used
+                      </strong>
+                    </Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+            </div>
+          </div>
+
+          <div className="card-body">
+            <div className="d-flex justify-content-center">
+              <Table className="table table-responsive table-striped w-auto rounded-top rounded-bottom border">
+                <Thead className="thead-dark">
+                  <Tr>
+                    <Th colspan="4" className="text-center">
+                      <strong>Parameters</strong>
+                    </Th>
+                  </Tr>
+                </Thead>
+                <Thead className="thead-light">
+                  <Tr>
+                    <Th>
+                      <strong>Parameter</strong>
+                    </Th>
+                    <Th>
+                      <strong>Type</strong>
+                    </Th>
+                    <Th>
+                      <strong>Required</strong>
+                    </Th>
+                    <Th>
+                      <strong>Description</strong>
+                    </Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td id="td_free_q_4">Authorization</Td>
+                    <Td id="td_free_q_5">Header</Td>
+                    <Td id="td_free_q_6">Yes</Td>
+                    <Td id="td_free_q_7">
+                      Value should be '
+                      <strong>Bearer &lt;your access_token&gt;</strong>'
+                    </Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+            </div>
+          </div>
+          <div className="card-body">
+            <div className="d-flex justify-content-center">
+              <Table className="table table-responsive table-striped w-auto rounded-top rounded-bottom border">
+                <Thead className="thead-dark">
+                  <Tr>
+                    <Th colspan="4" className="text-center">
+                      <strong>Response</strong>
+                    </Th>
+                  </Tr>
+                </Thead>
+                <Thead className="thead-light">
+                  <Tr>
+                    <Th>
+                      <strong>Property</strong>
+                    </Th>
+                    <Th>
+                      <strong>Type</strong>
+                    </Th>
+                    <Th colspan="2">
+                      <strong>Description</strong>
+                    </Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td id="td_free_q_8">status</Td>
+                    <Td id="td_free_q_9">
+                      <a href="#error_handling">Status</a>
+                    </Td>
+                    <Td id="td_free_q_10" colspan="2">
+                      Response <a href="#error_handling">status</a> object
+                    </Td>
+                  </Tr>
+                  <Tr>
+                    <Td id="td_free_q_11">queries</Td>
+                    <Td id="td_free_q_12">Long</Td>
+                    <Td id="td_free_q_13" colspan="2">
+                      Number of used queries out of current month's 10,000 FREE
+                      queries
+                    </Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+            </div>
+          </div>
+          {/* <!--Sample code to get number of used free queries in current month--> */}
+          <p id="p_free_q_2" className="mt-2 ml-4 mr-4 text-justify">
+            An example usage of this API is as follows:
+          </p>
+          <p id="p_free_q_3" className="mt-2 ml-4 mr-4 text-justify">
+            <code className="prettyprint">
+              curl -X GET 'https://aei.ai/api/v1/metrics/queries/used' -H
+              'Authorization: Bearer &lt;your access_token&gt;'
+            </code>
+          </p>
+          <p id="p_free_q_4" className="mt-2 ml-4 mr-4 text-justify">
+            Expected response from aEi.ai would be the same as the response
+            received when we got number of PAID queries in previous sections.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+FreeQueries.Layout = DocsLayout;
+export default FreeQueries;
